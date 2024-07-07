ab1427db253fa2e82f16eccb3904b97ee1f5604d
Ayushmaan sharma <shubhama22221@gmail.com>
2024-07-07 21:06:36 +0530

    array lists

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -1,19 +1,31 @@
 import java.util.*;
 import java.util.ArrayList;
-public class ArrayList {
+
+public class ArrayLists {
 
 
-    public static void main(String [] arg){
-        int i,n;
+    public static void main(String [] arg) {
+        int i, n;
         Scanner fg = new Scanner(System.in);
-        n=fg.nextInt();
+        System.out.println("the value of n is");
+        n = fg.nextInt();
         String[] str = new String[n];
-        for (i=0;i<n;i++){
-            str[i]=fg.next();
+        System.out.println("the values of string are: ");
+        for (i = 0; i < n; i++) {
+            str[i] = fg.next();
         }
-        for (String names : str){
-            System.out.print(names+" ");
+        for (String names : str) {
+            System.out.print(names + " ");
         }
+        System.out.println(" ");
         ArrayList<String> arr = new ArrayList<>();
+        arr.add("Simran sharma");
+        for (i = 0; i < n; i++) {
+            arr.add(str[i]);
+        }
+        System.out.println("Here is our array which take input from existing array and is dynamic in nature ");
+        for (i = 0; i < n+1; i++) {
+            System.out.print(arr.get(i)+" ");
+        }
     }
 }
